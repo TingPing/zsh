b7ad56284b9a37b4327fbbfc984e50613e5ee912
TingPing <tngpng@gmail.com>
2013-04-09 00:19:53 -0300

    Create insertdistrohere.plugin.zsh

---
+++ plugins/insertdistrohere.plugin.zsh
@@ -0,0 +1,11 @@
+# Auto-loads proper distro plugin
+
+if [ -f /etc/redhat-release ]; then
+  antigen-bundle yum
+elif [ -f /etc/debian_version ]; then
+	antigen-bundle debian
+elif [ -f /etc/arch-release ]; then
+	antigen-bundle archlinux
+elif [ -f /etc/SuSE-release ]; then
+	antigen-bundle suse
+fi
