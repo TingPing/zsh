be3594bef552154045e0eeade55143767f2845e3
TingPing <tngpng@gmail.com>
2013-04-13 04:29:54 -0300

    Create archlinux.plugin.zsh

---
+++ plugins/archlinux/archlinux.plugin.zsh
@@ -0,0 +1,20 @@
+# Archlinux zsh aliases and functions
+
+alias pup='sudo pacman -Syu'
+alias pin='sudo pacman -S'
+alias plin='sudo pacman -U'
+alias prm='sudo pacman -Rs'
+alias pinf='pacman -Si'
+alias plinf='pacman -Qi'
+alias pse='pacman -Ss'
+alias plse='pacman -Qs'
+alias plsor='sudo pacman -Qdt'
+alias prmor='sudo pacman -Rs $(pacman -Qtdq)'
+
+alias ppro='pkgfile'
+alias pls='pkgfile -l'
+
+alias ase='cower -s'
+alias ainf='cower -i -i'
+alias ain='cower -d'
+alias aup='cower -u'
