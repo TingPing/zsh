fa5ced37c6d26b77f1fe9e79137e62d6aa5c309b
TingPing <tngpng@gmail.com>
2013-04-13 17:57:07 -0300

    Create fedora.plugin.zsh

---
+++ plugins/fedora/fedora.plugin.zsh
@@ -0,0 +1,17 @@
+# Fedora zsh aliases and functions
+
+alias yse="yum search"
+alias ypro="yum provides"
+alias yinf="yum info"
+alias ygl="yum grouplist"
+alias yup="sudo yum update"
+alias yin="sudo yum install"
+alias ylin="sudo yum localinstall"
+alias ygin="sudo yum groupinstall"
+alias yrm="sudo yum remove"
+alias ygrm="sudo yum groupremove"
+
+# yum-utils
+alias ybd="sudo yum-builddep"
+alias yls="repoquery -l"
+alias yds="yumdownloader --source"
