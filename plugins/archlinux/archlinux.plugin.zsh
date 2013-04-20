f4dff6d6c83d6ac09675efb6dfb1720624574d2b
TingPing <tngpng@gmail.com>
2013-04-20 20:15:15 -0300

    Update archlinux.plugin.zsh

--- plugins/archlinux/archlinux.plugin.zsh
+++ plugins/archlinux/archlinux.plugin.zsh
@@ -11,13 +11,23 @@ alias plse='pacman -Qs'
 alias plsor='sudo pacman -Qdt'
 alias prmor='sudo pacman -Rs $(pacman -Qtdq)'
 
+if [ -f /usr/bin/pkgfile ];then
 alias ppro='pkgfile -di'
 alias pls='pkgfile -l'
+fi
 
-alias ase='cower -s'
-alias ainf='cower -ii'
-alias ain='cower -dd'
-alias aup='cower -u'
+if [ -f /usr/bin/meat ];then
+AURHELPER=meat
+elif [ -f /usr/bin/cower ];then
+AURHELPER=cower
+fi
+
+if [ -n "$AURHELPER" ];then
+alias ase="$AURHELPER -s"
+alias ainf="$AURHELPER -ii"
+alias ain="$AURHELPER -d"
+alias aup="$AURHELPER -u"
+fi
 
 alias mpi='makepkg -sif'
 alias mps='makepkg -Sf'
@@ -25,4 +35,6 @@ alias mpg='makepkg -g'
 alias mpd='makepkg -o'
 alias mpb='makepkg -e'
 
+if [ -f /usr/bin/burp ];then
 alias bup='burp' # just cuz =)
+fi
